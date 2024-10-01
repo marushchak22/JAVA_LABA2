a8be16e4bba3c29f7d9ec89002931eba8d9929ae
Nazar <marusaknazar0@gmail.com>
2024-10-01 22:52:19 +0300

    Jackson with records

--- src/main/java/laba_2/Main.java
+++ src/main/java/laba_2/Main.java
@@ -1,19 +1,27 @@
 package laba_2;
 
-import com.google.gson.Gson;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class Main {
     public static void main(String[] args) {
+
         Person person = new Person("Марущак", "Назар", 19);
 
-        Gson gson = new Gson();
-        String json = gson.toJson(person);
-        System.out.println("Серіалізований JSON: " + json);
+        ObjectMapper objectMapper = new ObjectMapper();
 
-        Person deserializedPerson = gson.fromJson(json, Person.class);
-        System.out.println("Десеріалізований об'єкт: " + deserializedPerson.getSurname() + " " + deserializedPerson.getName() + ", Вік: " + deserializedPerson.getAge());
+        try {
+            String json = objectMapper.writeValueAsString(person);
+            System.out.println("Серіалізований JSON: " + json);
 
-        boolean isEqual = person.equals(deserializedPerson);
-        System.out.println("Об'єкти рівні: " + isEqual);
+            Person deserializedPerson = objectMapper.readValue(json, Person.class);
+            System.out.println("Десеріалізований об'єкт: " + deserializedPerson.surname() + " " + deserializedPerson.name() + ", Вік: " + deserializedPerson.age());
+
+            boolean isEqual = person.equals(deserializedPerson);
+            System.out.println("Об'єкти рівні: " + isEqual);
+            
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
     }
 }
